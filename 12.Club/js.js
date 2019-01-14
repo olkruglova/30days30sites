ca88098403b6634b472503ab8219f99fba3db8d5
olkruglova <olkruhlova@gmail.com>
2019-01-14 18:38:51 +0100

    Day13: Wine site

--- 12.Club/js.js
+++ 12.Club/js.js
@@ -22,6 +22,61 @@ $('.team').on('click', function() {
 $('.gallery').on('click', function() {
     $('#gallery-section').css('margin-left', '0');
     $('#about-us, #training, #our-team, #contact').css('margin-left', '-80%');
+    $(function() {
+        var theImage = $('ul#gallery li img');
+        var theWidth = theImage.width()*1.1;
+        //wrap into mother div
+        $('ul#gallery').wrap('<div id="mother" />');
+        //assign height width and overflow hidden to mother
+        $('#mother').css({
+          width: function() {
+            return theWidth;
+          },
+          height: function() {
+            return theImage.height()*1.1;
+          },
+          position: 'relative',
+          overflow: 'hidden',
+          margin: '27% auto 0 auto',
+          boxShadow: '0 0 30px 1px #fff'
+        });
+        //get total of image sizes and set as width for ul 
+        var totalWidth = theImage.length * theWidth;
+        $('ul#gallery').css({
+          width: function() {
+            return totalWidth;
+          }
+        });
+      
+        var gallery_timer = setInterval(function() {
+          gallery_next();
+        }, 3000);
+      
+        function gallery_next() {
+          var a = $(".active");
+          a.removeClass('active');
+      
+          if (a.hasClass('last')) {
+            //last element -- loop
+            a.parent('ul#gallery').animate({
+              "margin-left": (0)
+            }, 1000);
+            a.siblings(":first").addClass('active');
+          } else {
+            a.parent('ul#gallery').animate({
+              "margin-left": (-(a.index() + 1) * theWidth)
+            }, 1000);
+            a.next().addClass('active');
+          }
+        }
+      
+        // Cancel slideshow and move next manually on click
+        $('ul#gallery li img').on('click', function() {
+          clearInterval(gallery_timer);
+          gallery_next();
+        });
+      
+      });
 })
 $('.contact').on('click', function() {
     $('#contact').css('margin-left', '0');
@@ -29,58 +84,3 @@ $('.contact').on('click', function() {
     $('#social').css('display', 'block').css('margin-top', '53%');
 })
 
-$(function() {
-    var theImage = $('ul#gallery li img');
-    var theWidth = theImage.width()*1.1;
-    //wrap into mother div
-    $('ul#gallery').wrap('<div id="mother" />');
-    //assign height width and overflow hidden to mother
-    $('#mother').css({
-      width: function() {
-        return theWidth;
-      },
-      height: function() {
-        return theImage.height()*1.1;
-      },
-      position: 'relative',
-      overflow: 'hidden',
-      margin: '27% auto 0 auto',
-      boxShadow: '0 0 30px 1px #fff'
-    });
-    //get total of image sizes and set as width for ul 
-    var totalWidth = theImage.length * theWidth;
-    $('ul#gallery').css({
-      width: function() {
-        return totalWidth;
-      }
-    });
-  
-    var gallery_timer = setInterval(function() {
-      gallery_next();
-    }, 3000);
-  
-    function gallery_next() {
-      var a = $(".active");
-      a.removeClass('active');
-  
-      if (a.hasClass('last')) {
-        //last element -- loop
-        a.parent('ul#gallery').animate({
-          "margin-left": (0)
-        }, 1000);
-        a.siblings(":first").addClass('active');
-      } else {
-        a.parent('ul#gallery').animate({
-          "margin-left": (-(a.index() + 1) * theWidth)
-        }, 1000);
-        a.next().addClass('active');
-      }
-    }
-  
-    // Cancel slideshow and move next manually on click
-    $('ul#gallery li img').on('click', function() {
-      clearInterval(gallery_timer);
-      gallery_next();
-    });
-  
-  });
